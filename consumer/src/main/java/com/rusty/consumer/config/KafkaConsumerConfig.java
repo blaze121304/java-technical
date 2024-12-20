8bdccddce5a484640bf364ce3263081a012ff0e2
rusty <blaze121304@gmail.com>
2024-12-20 17:46:49 +0900

    feat: 자바 테크니컬 북(가제) 1. kafka config 추가 (Consumer)

--- consumer/src/main/java/com/rusty/consumer/config/KafkaConsumerConfig.java
+++ consumer/src/main/java/com/rusty/consumer/config/KafkaConsumerConfig.java
@@ -1,4 +1,4 @@
-package com.rusty.api.config;
+package com.rusty.consumer.config;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.LongDeserializer;
